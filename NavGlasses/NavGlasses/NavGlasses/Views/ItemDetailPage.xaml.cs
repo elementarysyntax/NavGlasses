629e0747a1972853d9dfac9e17077e16b63a26f6
elementarysyntax <elementarysyntax@gmail.com>
2023-01-10 09:51:52 -0700

    Initial

---
+++ NavGlasses/NavGlasses/NavGlasses/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using NavGlasses.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace NavGlasses.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
