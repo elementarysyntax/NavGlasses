6c07911eb5c7407b04def7c1bf6a80098e905402
elementarysyntax <elementarysyntax@gmail.com>
2023-01-19 13:21:19 -0700

    Adds AndroidMessage

---
+++ NavGlasses/NavGlasses/NavGlasses.Android/Implementations/AndroidMessage.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NavGlasses.Interfaces;
+
+namespace NavGlasses.Droid.Implementations
+{
+    public class AndroidMessage : IMessage
+    {
+        public void LongAlert(string message)
+        {
+            Toast.MakeText(Application.Context, message, ToastLength.Long).Show();
+        }
+
+        public void ShortAlert(string message)
+        {
+            Toast.MakeText(Application.Context, message, ToastLength.Short).Show();
+        }
+    }
+}
