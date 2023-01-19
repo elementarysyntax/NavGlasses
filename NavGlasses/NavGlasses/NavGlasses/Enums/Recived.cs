0d10b68b376bb303cd2fa284d9d397339459ca14
elementarysyntax <elementarysyntax@gmail.com>
2023-01-19 11:11:02 -0700

    Adds enums

---
+++ NavGlasses/NavGlasses/NavGlasses/Enums/Recived.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavGlasses.Enums
+{
+    public enum Recived : int // TODO: change to something meeningful
+    {
+        REVERSE = 101
+    }
+}
