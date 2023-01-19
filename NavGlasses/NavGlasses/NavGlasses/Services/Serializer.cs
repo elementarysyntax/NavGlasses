e0041c49b9346f380bf2ae0ffdced92a42f55c41
elementarysyntax <elementarysyntax@gmail.com>
2023-01-19 11:17:02 -0700

    Adds Services.

---
+++ NavGlasses/NavGlasses/NavGlasses/Services/Serializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavGlasses.Services
+{
+    public static class Serializer
+    {
+        public static byte Serialize(int data)
+        {
+            return Convert.ToByte(data);
+        }
+    }
+}
